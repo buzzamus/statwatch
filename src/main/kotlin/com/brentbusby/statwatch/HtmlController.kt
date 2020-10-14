a5976732bf31289ff0451e0184c4c8c79df33b49
Brent Busby <brent.busby@yahoo.com>
2020-10-13 20:07:58 -0500

    add form, working call

--- src/main/kotlin/com/brentbusby/statwatch/HtmlController.kt
+++ src/main/kotlin/com/brentbusby/statwatch/HtmlController.kt
@@ -1,9 +1,13 @@
 package com.brentbusby.statwatch
 
+import com.brentbusby.statwatch.requests.UsernameRequest
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestParam
 
 @Controller
 class HtmlController {
@@ -17,5 +21,12 @@ class HtmlController {
     fun lookup(model: Model): String {
         model["title"] = "Stat lookup"
         return "lookup"
+    }
+
+    @PostMapping("/lookup")
+    fun lookupSubmit(@RequestParam("username") username: String): String {
+        var request = UsernameRequest()
+        var response = request.call(username)
+        return response.toString()
     }
 }
--- src/main/kotlin/com/brentbusby/statwatch/requests/UsernameRequest.kt
+++ src/main/kotlin/com/brentbusby/statwatch/requests/UsernameRequest.kt
@@ -6,13 +6,12 @@ import okhttp3.Response
 
 class UsernameRequest() {
 
-    fun call(username: String, accountType: String) {
+    fun call(username: String) {
         val response: Response = httpGet {
             host = "fortnite-api.com"
             path = "/v1/stats/br/v2"
             param {
                 "name" to username
-                "accountType" to accountType
             }
         }
 
