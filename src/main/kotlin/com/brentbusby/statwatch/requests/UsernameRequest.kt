8404f5d7a146c2dcdb47f4b66738b42a15423b59
Brent Busby <brent.busby@yahoo.com>
2021-03-21 13:36:44 -0500

    cleanup

--- src/main/kotlin/com/brentbusby/statwatch/requests/UsernameRequest.kt
+++ src/main/kotlin/com/brentbusby/statwatch/requests/UsernameRequest.kt
@@ -8,9 +8,6 @@ import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.context.annotation.Configuration
 import org.springframework.stereotype.Service
 
 @Service
